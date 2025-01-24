e3027ce1877d6b5d001bdcd8d96f60659d59e41a
chengzeyi <ichengzeyi@gmail.com>
2025-01-24 16:08:08 +0800

    fix

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -97,7 +97,6 @@ def test_benchmark_fp8_attn_func(D, dtype, device, is_causal):
         import triton.profiler.viewer as proton_viewer
 
         metrics = ["time/ms"]
-        metrics = ["tflop8/s", "tflop16/s"] + metrics
         file_name = f"{profile_name}.hatchet"
         proton_viewer.parse(metrics, file_name, depth=100)
 
